cffdf1fc41cf68fdaca4d32be40d35b85c1298e6
Aditya Panchal <50858157+adityapanchal10@users.noreply.github.com>
2021-05-25 13:20:50 +0530

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -5,7 +5,9 @@ module.exports = function (msg, args) {
 		serverQueue.playing = true;
 		serverQueue.songs = [];
 		serverQueue.i = 0;
+		serverQueue.jump = -1;
 		msg.client.id = 1;
+		msg.client.shuffle = false;
 		return serverQueue.textChannel.send("Queue cleared!");
 	}
 	return msg.channel.send("There is nothing playing.");
